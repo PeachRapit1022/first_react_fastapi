3b7666adaeb2986b79ec05b3e3b0be838a61cb95
PeachRapit1022 <ia.rapit22@gmail.com>
2022-12-14 22:42:23 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import axios from "axios";
+import { Routes, Route, Link } from 'react-router-dom';
+
+import Home from './routes/home';
+import TestGet from './routes/testGet';
+import TestPost from './routes/testPost';
+import TestPut from './routes/testPut';
+import TestDelete from './routes/testDelete';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <h1>React Memo App Demo</h1>
+            <ul>
+                <li>
+                    <Link to={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link to={"/get"}>testGet</Link>
+                </li>
+                <li>
+                    <Link to={"/post"}>testPost</Link>
+                </li>
+                <li>
+                    <Link to={"/put"}>testPut</Link>
+                </li>
+                <li>
+                    <Link to={"/delete"}>testDelete</Link>
+                </li>
+            </ul>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/get" element={<TestGet />} />
+                <Route path="/post" element={<TestPost />} />
+                <Route path="/put" element={<TestPut />} />
+                <Route path="/delete" element={<TestDelete />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
