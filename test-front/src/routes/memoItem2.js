2429dd75ffdccae0ae08ac3016fee3fdf42d701a
PeachRapit1022 <ia.rapit22@gmail.com>
2022-12-19 18:47:58 +0900

    コメント整理

--- test-front/src/routes/memoItem2.js
+++ test-front/src/routes/memoItem2.js
@@ -5,24 +5,20 @@ const baseURL = "http://localhost:8000"
 
 const MemoItem = (props) => {
 
+    // アイテムの削除
     const [isAlive, setAlive] = React.useState(true);
     const deletePost = (id) => {
         setAlive(false);
         axios.delete(`${baseURL}/delete/${id}`);
     }
 
+    // アイテムの更新
     const changePost = (newItem) => {
         axios.put(`${baseURL}/edit/${newItem.id}`,{newItem})
         .then((response) => {console.log(response.data)})
     }
 
-
-    const [isModalOpen, setModal] = React.useState(false);
-    const handleClickModal = (isModalOpen) => {
-        setModal(!isModalOpen);
-        console.log(isModalOpen, props.id);
-    }
-
+    // 更新アイテムの取得
     let newItem;
     const getNewItem = () => {
 
@@ -35,6 +31,14 @@ const MemoItem = (props) => {
 
     }
 
+    // モーダルの状態保持と状態変更
+    const [isModalOpen, setModal] = React.useState(false);
+    const handleClickModal = (isModalOpen) => {
+        setModal(!isModalOpen);
+        console.log(isModalOpen, props.id);
+    }
+
+    // モーダルの定義
     let modal;
     if (isModalOpen) {
         modal = (
@@ -46,26 +50,23 @@ const MemoItem = (props) => {
         );
     }
 
+    // メモアイテムの定義
     let memoItem;
     if (isAlive) {
         memoItem = (
         <>
-
-                <span>{props.id}</span>
-                <span>{props.title}</span>
-                <div>{props.body}</div>
-
-                    <button onClick={() => handleClickModal(isModalOpen)}>編集</button>
-
-                    <button onClick={() => deletePost(props.id)}>削除</button>
-
-
+            <span>{props.id}</span>
+            <span>{props.title}</span>
+            <div>{props.body}</div>
+            <button onClick={() => handleClickModal(isModalOpen)}>編集</button>
+            <button onClick={() => deletePost(props.id)}>削除</button>
         </>
         );
     } else {
         memoItem = <tr>deleted</tr>
     }
     
+    // 出力
     return (
         <>
             {memoItem}
